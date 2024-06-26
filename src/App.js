3f709cdbe276961eada649829bd760e83773b561
Arma Burton <arma@armaburton.com>
2024-06-25 20:23:37 -0700

    {button clicks}

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 
 function App() {
+
+  const handleClick = (e) => {
+    const value = e.target.value;
+    console.log("click");
+    console.log(value);
+    alert(value);
+  }
+
   return (
     <section className="App">
       <div id="buttonWrapper">
-        <button className="gamePad" id="blue"></button>
-        <button className="gamePad" id="red"></button>
-        <button className="gamePad" id="yellow"></button>
-        <button className="gamePad" id="green"></button>
+        <button className="gamePad" onClick={(e) => handleClick(e)} key="blue" value="blue" id="blue"></button>
+        <button className="gamePad" onClick={(e) => handleClick(e)} value="red" id="red"></button>
+        <button className="gamePad" onClick={(e) => handleClick(e)} value="yellow" id="yellow"></button>
+        <button className="gamePad" onClick={(e) => handleClick(e)} value="green" id="green"></button>
       </div>
     </section>
   );
