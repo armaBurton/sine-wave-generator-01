e876a34f2410862a1ad5a1091552128cb772710b
Arma Burton <arma@armaburton.com>
2024-06-25 20:56:32 -0700

    {oscillations are working}

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
 
 function App() {
+  const context = new AudioContext();
 
   const handleClick = (e) => {
     const value = e.target.value;
-    console.log("click");
-    console.log(value);
-    alert(value);
+
+    const freq = (value === "blue") ? 195.998 :
+      (value === "yellow") ? 261.626 : 
+        (value === "red") ? 329.628 : 391.995;
+
+    playOscillator(context.currentTime, context.currentTime + .42, freq)
+  }
+
+  const playOscillator = (startTime, endTime, freq) =>{
+    const oscillator = context.createOscillator();
+    const gain = context.createGain();
+
+    oscillator.connect(gain);
+    oscillator.type = 'square';
+    oscillator.frequency.value = freq;
+    gain.gain.value = 0.125;
+    gain.connect(context.destination);
+    oscillator.start(startTime);
+    // gain.gain.exponentialRampToValueAtTime(0.1, endTime);
+    oscillator.stop(endTime);
   }
 
   return (
